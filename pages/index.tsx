ef78f06c4f14aca7bcf502dbe6eb0170eb2a5969
Jacob McMichael <jacobmcmichael@gmail.com>
2022-08-30 12:02:16 -0700

    [MODIFY]: Change to SSR with ApolloClient
    Signed-off-by: Jacob McMichael <jacobmcmichael@gmail.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,41 +2,42 @@ import type { GetServerSideProps, NextPage } from 'next';
 import { gql, useQuery } from '@apollo/client';
 import Image from 'next/image';
 
-import { prisma } from '../prisma/prisma';
-
+import apolloClient from '../apollo/apollo';
 import styles from '../styles/Home.module.css';
 
-const UsersQuery = gql`
+const GET_ALL_USERS = gql`
 	query {
 		users {
-			id
 			username
 			img
 		}
 	}
 `;
-
 interface User {
-	id: string
-	email: string
-	firstName: string
-	lastName: string
-	phone: string
-	img: string
-	username: string
+	id: string;
+	email: string;
+	firstName: string;
+	lastName: string;
+	phone: string;
+	img: string;
+	username: string;
 }
 
 const Home: NextPage<any> = (props) => {
-	const { data, error, loading } = useQuery(UsersQuery);
-
-	if (loading) return <p>Loading...</p>;
-	if (error) return <p>Oops, something went wrong {error.message}...</p>;
+	if (props.loading) return <p>Loading...</p>;
+	if (props.error)
+		return <p>Oops, something went wrong {props.error.message}...</p>;
 
 	return (
 		<div>
-			{data.users.map((user: User) => (
+			{props.data.users.map((user: User) => (
 				<div key={user.id}>
-					<Image src={user.img} alt={`${user}'s image`} width={50} height={50} />
+					<Image
+						src={user.img}
+						alt={`${user}'s image`}
+						width={50}
+						height={50}
+					/>
 					<p>{user.username}</p>
 				</div>
 			))}
@@ -44,14 +45,16 @@ const Home: NextPage<any> = (props) => {
 	);
 };
 
-// export const getServerSideProps: GetServerSideProps = async () => {
-// 	const users = await prisma.user.findMany();
+export const getServerSideProps: GetServerSideProps = async () => {
+	const { data } = await apolloClient.query({
+		query: GET_ALL_USERS,
+	});
 
-// 	return {
-// 		props: {
-// 			users: JSON.parse(JSON.stringify(users)),
-// 		},
-// 	};
-// };
+	return {
+		props: {
+			data,
+		},
+	};
+};
 
 export default Home;
